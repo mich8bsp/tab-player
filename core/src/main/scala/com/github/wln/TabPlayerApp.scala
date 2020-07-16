1075d4eed938b4c711b378818d7a200cf77cdf5e
Michael Bespalov <mich8bsp@gmail.com>
2020-07-16 20:15:26 +0300

    file parsing, merging logic needs further work

--- core/src/main/scala/com/github/wln/TabPlayerApp.scala
+++ core/src/main/scala/com/github/wln/TabPlayerApp.scala
@@ -1,7 +1,16 @@
 package com.github.wln
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.github.wln.parsing.TabFileParser
+
+import scala.io.Source
 
 class TabPlayerApp extends ApplicationAdapter{
+
+  override def create(): Unit = {
+    val parser = new TabFileParser()
+    val tab = parser.parse(Source.fromResource("take_on_me.tabs"))
+    println(tab)
+  }
 
 }
--- core/src/main/scala/com/github/wln/parsing/TabFileParser.scala
+++ core/src/main/scala/com/github/wln/parsing/TabFileParser.scala
@@ -8,56 +8,88 @@ import scala.util.{Success, Try}
 
 class TabFileParser {
 
-  def parse(filePath: URI): SongTab = {
-    val fileSource = Source.fromFile(filePath)
+  def parse(fileSource: Source): SongTab = {
     val lines = fileSource.getLines()
-    val tab = buildTabFromLines(lines)
+    val builder = new TabBuilder()
+    lines.foreach(line => builder.addLine(line))
+    val tab = builder.build
     fileSource.close()
     tab
   }
 
-  def buildTabFromLines(lines: Iterator[String]): SongTab = {
-    var tab: SongTab = SongTab()
-    val songPartsByName: mutable.Map[String, SongPart] = mutable.Map()
-    var currentSongPart: Option[SongPart] = None
-    var processingChords: Boolean = false
-    lines.foreach {
-      case l if l.contains("[Title]") => tab = tab.copy(title = Some(l.replaceFirst("\\[Title\\]", "").trim))
-      case l if l.contains("[Artist]") => tab = tab.copy(artist = Some(l.replaceFirst("\\[Artist\\]", "").trim))
-      case l if l.startsWith("[Chords]") => {
-        currentSongPart.foreach(tab.addSongPart)
-        currentSongPart = None
-        processingChords = true
-      }
-      case l if l.startsWith("\\[") => {
-        processingChords = false
-        currentSongPart.foreach(tab.addSongPart)
-        val partName = l.substring(1, l.indexOf("\\]"))
-        val songPart = songPartsByName.getOrElseUpdate(partName, SongPart(partName = partName))
-        currentSongPart = Some(SongPart(songPart.partName, mutable.Buffer(songPart.lines)))
-      }
-      case l if l.trim.isEmpty => currentSongPart.foreach(p => p.addLine(EmptyLine))
-      case l if processingChords => {
-        val Array(chordName, chordTab, _*) = l.split("-").map(_.trim)
-        tab.chordsRegistry.addChord(parseChord(chordName), chordTab)
-      }
-      case l => {
-        currentSongPart.foreach(p => p.addLine(parseLine(l)))
-      }
+
+}
+
+class TabBuilder {
+
+  import TabParser._
+
+  var chordsRegistry = ChordsRegistry()
+  var tab: SongTab = SongTab(chordsRegistry = chordsRegistry)
+  val songPartsByName: mutable.Map[String, SongPart] = mutable.Map()
+  var currentSongPart: Option[SongPart] = None
+  var processingChords: Boolean = false
+
+  def addLine(line: String): Unit = line match {
+    case l if l.contains("[Title]") => tab = tab.copy(title = Some(l.replaceFirst("\\[Title\\]", "").trim))
+    case l if l.contains("[Artist]") => tab = tab.copy(artist = Some(l.replaceFirst("\\[Artist\\]", "").trim))
+    case l if l.startsWith("[Chords]") => {
+      finalizeCurrentPart()
+      processingChords = true
     }
-    currentSongPart.foreach(tab.addSongPart)
+    case l if l.startsWith("[") => {
+      finalizeCurrentPart()
+      val partName = l.substring(1, l.indexOf("]"))
+      currentSongPart = Some(SongPart(partName = partName))
+    }
+    case l if l.trim.isEmpty => currentSongPart.foreach(p => p.addLine(EmptyLine))
+    case l if processingChords => {
+      val Array(chordName, chordTab, _*) = l.split("-").map(_.trim)
+      chordsRegistry.addChord(parseChord(chordName), chordTab)
+    }
+    case l => {
+      currentSongPart.foreach(p => p.addLine(parseLine(l)))
+    }
+  }
+
+  def finalizeCurrentPart(): Unit = {
+    processingChords = false
+    val repeatingPart: Option[SongPart] = currentSongPart.map(_.partName).flatMap(songPartsByName.get)
+
+    val partToAdd: Option[SongPart] = (for {
+      previousPartOfSameName <- repeatingPart
+      currPart <- currentSongPart
+    }yield {
+      mergeParts(previousPartOfSameName, currPart)
+    }).orElse(currentSongPart)
+
+    partToAdd.foreach(tab.addSongPart)
+
+    if(repeatingPart.isEmpty && currentSongPart.nonEmpty){
+      songPartsByName.put(currentSongPart.get.partName, currentSongPart.get)
+    }
+    currentSongPart = None
+  }
+
+  def build: SongTab = {
+    finalizeCurrentPart()
     tab
   }
 
-  private def parseLine(line: String): ITabLine = {
+}
+
+object TabParser {
+  import scala.language.postfixOps
+
+  def parseLine(line: String): ITabLine = {
     val lineParts = line.split("[ \t]")
       .map(_.trim)
       .filter(_.nonEmpty)
       .toSeq
 
-    if(lineParts.isEmpty){
+    if (lineParts.isEmpty) {
       EmptyLine
-    }else{
+    } else {
       Try(lineParts.map(parseChord)) match {
         case Success(v) => ChordsLine(line, v)
         case _ => LyricsLine(line)
@@ -65,17 +97,17 @@ class TabFileParser {
     }
   }
 
-  private def parseChord(str: String): Chord = {
+  def parseChord(str: String): Chord = {
     var chordStrToParse: String = str
     val root: Note.Value = parseNote(chordStrToParse)
     chordStrToParse = chordStrToParse.substring(root.toString.length, chordStrToParse.length)
     val chordQuality: ChordQuality.Value = parseChordQuality(chordStrToParse)
     chordStrToParse = removeChordQuality(chordStrToParse)
 
-    val (chordExtension, altRoot): (String, Option[Note.Value]) = if(chordStrToParse.contains("/")){
+    val (chordExtension, altRoot): (String, Option[Note.Value]) = if (chordStrToParse.contains("/")) {
       val Array(ext, altRootStr) = chordStrToParse.split("/")
       (ext, Some(parseNote(altRootStr)))
-    }else{
+    } else {
       (chordStrToParse, None)
     }
 
@@ -84,13 +116,13 @@ class TabFileParser {
 
   private def parseNote(str: String): Note.Value = {
     val note: Note.Value = Note.withName(str.substring(0, 1).toUpperCase)
-    if(str.length > 1){
+    if (str.length > 1) {
       str(1) match {
         case '#' => note sh
-        case "b" => note fl
+        case 'b' => note fl
         case _ => note
       }
-    }else{
+    } else {
       note
     }
   }
@@ -103,12 +135,54 @@ class TabFileParser {
   }
 
   private def removeChordQuality(str: String): String = {
-    if(str.length >= 3 && Set("maj", "min", "aud", "dim").contains(str.substring(0, 3).toLowerCase())){
+    if (str.length >= 3 && Set("maj", "min", "aud", "dim").contains(str.substring(0, 3).toLowerCase())) {
       str.substring(3, str.length)
-    }else if(str.nonEmpty && str.head.toLower == 'm'){
+    } else if (str.nonEmpty && str.head.toLower == 'm') {
       str.tail
-    }else{
+    } else {
       str
+    }
+  }
+
+  def mergeParts(previousPart: SongPart, currentPart: SongPart): SongPart = {
+    if(currentPart.lines.isEmpty){
+      previousPart
+    }else{
+      var prevLinesIdx: Int = 0
+      var currLinesIdx: Int = 0
+      val prevPartLines: mutable.Buffer[ITabLine] = previousPart.lines
+      val currPartLines: mutable.Buffer[ITabLine] = currentPart.lines
+      val mergeBuffer: mutable.Buffer[ITabLine] = mutable.Buffer()
+
+      while(prevLinesIdx < prevPartLines.size && currLinesIdx < currPartLines.size){
+        val prevLine: ITabLine = prevPartLines(prevLinesIdx)
+        val currLine: ITabLine = currPartLines(currLinesIdx)
+
+        val currIsReplacing: Boolean = (prevLine, currLine) match {
+          case (EmptyLine, EmptyLine) => true
+          case (ChordsLine(_, _), ChordsLine(_, _)) => true
+          case (LyricsLine(_), LyricsLine(_)) => true
+          case _ => false
+        }
+
+        if(currIsReplacing){
+          mergeBuffer.addOne(currLine)
+          prevLinesIdx+=1
+          currLinesIdx+=1
+        }else{
+          mergeBuffer.addOne(prevLine)
+          prevLinesIdx+=1
+        }
+      }
+
+      if(prevLinesIdx < prevPartLines.size){
+        mergeBuffer.addAll(prevPartLines.toSeq.drop(prevLinesIdx))
+      }
+      if(currLinesIdx < currPartLines.size){
+        mergeBuffer.addAll(currPartLines.toSeq.drop(currLinesIdx))
+      }
+
+      SongPart(currentPart.partName, mergeBuffer)
     }
   }
 }
